5f7016c6365b151fc81cffa4bab3a7ec8241476d
Jan-Philipp <japhib@outlook.com>
2018-08-17 15:55:15 -0700

    last commit

--- src/components/EmotionBar.js
+++ src/components/EmotionBar.js
@@ -89,28 +89,51 @@ class Emotion extends React.Component {
            </Header>
          </header>
            <div className="emotion main">
-             <div className="emoji div">
-               <Segment as={Button} circular textAlign="center" className={this.renderColor(1)} onClick={() => this.thumbsUp()}>
-                 <Emoji  emoji='thumbsup' set='apple' skin="6" size={36} />
-               </Segment>
-             </div>
-             <div className="emoji div">
-               <Segment as={Button} circular raised textAlign="center" className={this.renderColor(0)} onClick={() => this.okay()}>
-                 <Emoji emoji="ok_hand" set='apple' skin="4" size={36}  />
-               </Segment>
-             </div>
-             <div className="emoji div">
-               <Segment  as={Button} circular  onClick={() => this.thumbsDown()}  raised textAlign="center" className={this.renderColor(-1)}>
-                 <Emoji emoji='thumbsdown' set='apple' skin="4" size={36}  />
-               </Segment>
-             </div>
-             <div className="emoji div">
-               <Segment as={Button} circular onClick={() => this.confused()} raised textAlign="center" className={this.renderColor(-2)}>
-                 <Emoji emoji='exploding_head' set='apple' skin='5' size={36} />
-               </Segment>
-             </div>
+             <Segment as={Button} circular size="big" textAlign="center" className={this.renderColor(1)} onClick={() => this.thumbsUp()}>
+               <Emoji  emoji='thumbsup' set='apple' skin="6" size={60} />
+             </Segment>
+             <Segment as={Button} circular size="big" raised textAlign="center" className={this.renderColor(0)} onClick={() => this.okay()}>
+               <Emoji emoji="ok_hand" set='apple' skin="4" size={60} />
+             </Segment>
+             <Segment  as={Button} circular size="big"  onClick={() => this.thumbsDown()}  raised textAlign="center" className={this.renderColor(-1)}>
+               <Emoji emoji='thumbsdown' set='apple' skin="4" size={60} />
+             </Segment>
+             <Segment as={Button} circular size="big" onClick={() => this.confused()} raised textAlign="center" className={this.renderColor(-2)}>
+               <Emoji emoji='exploding_head' set='apple' skin='5' size={60} />
+             </Segment>
            </div>
       </div>
+
+      // <div className="emotiongrid wrapper">
+      //    <header class="header">
+      //      <Header  className="commentblock" as='h1' textAlign="center">
+      //        Emotion Bar
+      //        <Header.Subheader content='What is your understanding of the current situation?' />
+      //      </Header>
+      //    </header>
+      //      <div className="emotion main">
+      //        <div className="emoji div">
+      //          <Segment as={Button} circular textAlign="center" className={this.renderColor(1)} onClick={() => this.thumbsUp()}>
+      //            <Emoji  emoji='thumbsup' set='apple' skin="6" size={36} />
+      //          </Segment>
+      //        </div>
+      //        <div className="emoji div">
+      //          <Segment as={Button} circular raised textAlign="center" className={this.renderColor(0)} onClick={() => this.okay()}>
+      //            <Emoji emoji="ok_hand" set='apple' skin="4" size={36}  />
+      //          </Segment>
+      //        </div>
+      //        <div className="emoji div">
+      //          <Segment  as={Button} circular  onClick={() => this.thumbsDown()}  raised textAlign="center" className={this.renderColor(-1)}>
+      //            <Emoji emoji='thumbsdown' set='apple' skin="4" size={36}  />
+      //          </Segment>
+      //        </div>
+      //        <div className="emoji div">
+      //          <Segment as={Button} circular onClick={() => this.confused()} raised textAlign="center" className={this.renderColor(-2)}>
+      //            <Emoji emoji='exploding_head' set='apple' skin='5' size={36} />
+      //          </Segment>
+      //        </div>
+      //      </div>
+      // </div>
 
     );
   }
