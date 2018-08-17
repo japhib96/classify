bf1c046f159297dcf951fc6aabf7c22fcb061eeb
akirk47 <andrewkirk093@gmail.com>
2018-08-17 14:21:12 -0700

    feedback on backend is done

--- src/components/testFrontend/studentStats.js
+++ src/components/testFrontend/studentStats.js
@@ -13,7 +13,7 @@ export default class StudentStats extends React.Component {
     this.state = {
       averageReaction: 0,
       topQuestions: [],
-      userQuestions: [],
+      confusedSlides: [],
       loading: false
     };
   }
@@ -24,26 +24,26 @@ export default class StudentStats extends React.Component {
   }
 
 async getFeedBack(){
-    await axios.post('/getFeedback/teacher', {
+    await axios.post('/getFeedback/student', {
       lectureId : this.props.lecture.id,
       userId: this.props.user._id
     })
     .then((resp) => {
       console.log(resp)
-      this.setState({ averageReaction: resp.data.averageReaction, topQuestions: resp.data.topQuestions, numberOfQuestions: resp.data.numberOfQuestions, loading: false  })
+      this.setState({ averageReaction: resp.data.averageReaction, topQuestions: resp.data.topQuestions, confusedSlides: resp.data.confusedSlides, loading: false  })
     }).catch((e)=>{
       alert(e)
     });
   }
 
 render (){
-
+  if (this.state.loading) { return <Loading message={'Creating Feedback...'}/> }
   return(
 
     <Container text textAlign='justified' style={{height: 500, padding: 50, backgroundColor: 'white'}}>
       <h1 align='center'>Feedback</h1>
 
-      <h4 align='left'>Most popular questions:</h4>
+      <h4 align='left'>Overall Understanding:  {this.state.averageReaction}</h4>
 
       <List ordered>
         <List.Item icon='thumbs up' content='Q1'/>
@@ -51,18 +51,18 @@ render (){
         <List.Item icon='thumbs down' content='Q3'/>
       </List>
 
-      <h4 align='left'>Where you were most confused:</h4>
+      <h4 align='left'>Slides you were confused on:</h4>
       <List ordered>
-        <List.Item content='Slide 1'/>
-        <List.Item  content='Slide two'/>
-        <List.Item  content='Slide three'/>
+        {this.state.confusedSlides.map( (slideNum, index) =>{
+          return (<List.Item>Slide {slideNum}</List.Item>)
+        })}
       </List>
 
       <h4 align='left'>Your questions:</h4>
       <List ordered>
-        <List.Item content='Q1'/>
-        <List.Item  content='Q2'/>
-        <List.Item  content='Q3'/>
+        {this.state.topQuestions.map( (question, index) =>{
+          return (<List.Item>{question.author}: {question.message}</List.Item>)
+        })}
       </List>
 
     </Container>
